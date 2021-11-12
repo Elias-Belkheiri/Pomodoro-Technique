4dd1a797c5615416e7caea0ec82d9aa390dfdcb9
Elias-Belkheiri <ilyasseyasser3@gmail.com>
2021-11-12 20:59:01 +0100

    'Up'

--- actions.js
+++ actions.js
@@ -4,18 +4,54 @@ let choices = document.querySelectorAll('.choice');
 let body = document.body;
 let doc = document.documentElement.style;
 
+
+
+let addremove = () => {
+    for (let i = 0; i < clas.length; i++) {
+        clas[i].classList.remove('back-color')
+    }
+}
+
+
 // Actions
 
-let backcolor1 = () => {
+function backcolor1 () {
     doc.cssText = "--main: #77937D; --secondry: #597478";
+    // clas.classList.remove('back-color');
+    let clas = document.querySelectorAll('.back-color')
+    let addremove = () => {
+        for (let i = 0; i < clas.length; i++) {
+            clas[i].classList.remove('back-color')
+        }
+    }
+    addremove()
+    this.classList.add('back-color');
 }
 
-let backcolor2 = () => {
+function backcolor2 () {
     doc.cssText = "--main: #2d92a7; --secondry: #267888";
+    // clas.classList.remove('back-color');
+    let clas = document.querySelectorAll('.back-color')
+    let addremove = () => {
+        for (let i = 0; i < clas.length; i++) {
+            clas[i].classList.remove('back-color')
+        }
+    }
+    addremove()
+    this.classList.add('back-color');
 }
 
-let backcolor3 = () => {
+function backcolor3 () {
     doc.cssText = "--main: #FFA384; --secondry: #d68c74";
+    // clas.classList.remove('back-color');
+    let clas = document.querySelectorAll('.back-color')
+    let addremove = () => {
+        for (let i = 0; i < clas.length; i++) {
+            clas[i].classList.remove('back-color')
+        }
+    }
+    addremove()
+    this.classList.add('back-color');
 }
 
 choices[0].onclick = backcolor1;
